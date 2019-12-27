5594c3e14e7b47c4990c1c415ecc11a7a4e998bb
John Zhao <joh.zhao@gmail.com>
2019-12-27 09:29:57 +0800

    remove useless package

--- test/test_css_unpack.py
+++ test/test_css_unpack.py
@@ -1,7 +1,5 @@
 import logging
 
-import pytest
-
 from dianping.css_unpack.css_manager import CSSManager
 from dianping.css_unpack.css_unpacker import CSSUnpacker
 
@@ -12,13 +10,12 @@ logger.addHandler(logging.NullHandler())
 
 
 def test_css_manager():
-    # m = CSSManager()
-    # url = '//s3plus.meituan.net/v1/mss_0a06a471f9514fc79c981b5466f56b91/svgtextcss/47637d8eb34f013f3b3c2c997246a587.css'
-    # p = m.get_css_unpacker(url)
-    pass
+    m = CSSManager()
+    url = '//s3plus.meituan.net/v1/mss_0a06a471f9514fc79c981b5466f56b91/svgtextcss/47637d8eb34f013f3b3c2c997246a587.css'
+    p = m.get_css_unpacker(url)
+    assert p is not None
 
 
-@pytest.mark.skip
 def test_css_unpacker():
     manager = CSSManager()
     with open('./examples/output_01.css', 'r') as css_file:
